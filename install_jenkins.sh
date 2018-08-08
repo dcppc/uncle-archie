46190d228c0036ff47a6b21b288a5653fa6d10b7
Charles Reid <charlesreid1@gmail.com>
2018-08-08 09:40:23 -0700

    remove jenkins install script

--- install_jenkins.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
-echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
-
-sudo apt-get update
-sudo apt-get install jenkins
