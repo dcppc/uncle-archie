c9eac60d2f27601544b2f1b5871fc7a6b3813d51
Charles Reid <charlesreid1@gmail.com>
2018-08-08 13:47:09 -0700

    update hook name to just_print

--- process_payload.py
+++ process_payload.py
@@ -1,7 +1,7 @@
-from hooks.private_www import process_payload as private_www
+from hooks.just_print import process_payload as just_print
 
 def process_payload(payload,meta):
-    private_www(payload,meta)
+    just_print(payload,meta)
 
 if __name__=="__main__":
     process_payload({'type':'test','name':'process_payload'},{'a':1,'b':2})
