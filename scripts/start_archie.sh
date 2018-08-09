889497c0f1ed8105eb60053d0be04ba94406a7e5
Charles Reid <charlesreid1@gmail.com>
2018-08-09 13:53:37 -0700

    add startup script for Uncle Archie. complicated.

---
+++ scripts/start_archie.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Shell script used to start up uncle archie
+# so uncle archie can run as a startup service.
+# 
+# This is reliant on having pyenv set up already
+# on beavo, the server that runs archie.
+
+ARCHIE_DIR="/home/ubuntu/uncle-archie"
+PYENV_BIN="/home/ubuntu/.pyenv/bin"
+
+echo "Preparing python"
+eval "$(${PYENV_BIN}/pyenv init -)"
+
+echo "Changing directories"
+cd ${ARCHIE_DIR}
+
+echo "Installing virtualenv"
+virtualenv vp
+source vp/bin/activate
+
+echo "Running Uncle Archie in a screen"
+screen -S archie -d -m bash -c "python ${ARCHIE_DIR}/uncle_archie.py; tail -f /dev/null"
+
