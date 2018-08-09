5ee628204753ab198f756a4fd725fb0dcc47a620
Charles Reid <charlesreid1@gmail.com>
2018-08-09 01:22:50 -0700

    just print hook now dumps json payload to timestamped file

--- hooks/just_print.py
+++ hooks/just_print.py
@@ -3,12 +3,10 @@ just_print CI Hook for Uncle Archie
 """
 
 def process_payload(payload,meta):
-    print("*"*40)
-    print(payload)
-    print("*"*40)
-    print(meta)
-    print("*"*40)
-    print("\n")
+    from datetime import datetime
+    fname = datetime.now().isoformat()
+    with open('/tmp/%s'%(fname),'w') as f:
+        f.write(json.dumps(payload,indent=4))
 
 if __name__=="__main__":
     process_payload({'type':'test','name':'private_www'},{'a':1,'b':2})
