80bf452bd561ac3b489fb611cc88b15f240c64f6
Charles Reid <charlesreid1@gmail.com>
2018-08-10 14:33:35 -0700

    add test_mkdocs to process payload

--- process_payload.py
+++ process_payload.py
@@ -1,9 +1,11 @@
 from hooks.dump_payload import process_payload as dump_payload
 from hooks.rubber_stamp import process_payload as rubber_stamp
+from hooks.test_mkdocs  import process_payload as test_mkdocs
 
 def process_payload(payload,meta,config):
     dump_payload(payload,meta,config)
     rubber_stamp(payload,meta,config)
+    test_mkdocs( payload,meta,config)
 
 if __name__=="__main__":
     process_payload({'type':'test','name':'process_payload'},{'a':1,'b':2})
