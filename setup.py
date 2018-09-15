4fc2192ac6c74c2620652ecf52279f4c7cb10474
Charles Reid <charlesreid1@gmail.com>
2018-09-15 09:35:38 -0700

    add setup.py and manifest.in file

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+
+"""
+setup.py for Uncle Archie
+
+Continuous integration (CI) testing tool for DCPPC.
+"""
+
+with open('requirements.txt') as f:
+    required = [x for x in f.read().splitlines() if not x.startswith("#")]
+
+# Note: the _program variable is set in __init__.py.
+# it determines the name of the package/final command line tool.
+from src import __version__
+
+config = {
+    'name': 'archie',
+    'description': 'Uncle Archie is a home-brewed continuous integration server',
+    'url': 'https://pages.charlesreid1.com/uncle-archie',
+    'author': 'Charles Reid',
+    'version' : __version__,
+    'install_requires': required,
+    'include_package_data' : True,
+    'test_suite': 'nose.collector',
+    'tests_require': ['nose'],
+    'packages': [
+        'archie',
+        'archie.webapp',
+        'archie.payload_handlers',
+        'archie.tasks',
+    ],
+    'package_dir' : {
+        'archie' :                 'src',
+        'archie.webapp' :          'src/webapp',
+        'archie.payload_handlers': 'src/payload_handlers',
+        'archie.tasks'  :          'src/tasks',
+    },
+    'scripts': [],
+    'zip_safe' : False
+}
+
+setup(**config)
