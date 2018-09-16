4636f6cffab36207244a1624d6148bbc0bee868f
Charles Reid <charlesreid1@gmail.com>
2018-09-16 12:36:24 -0700

    import all handler classes, and make available via __all__

--- src/payload_handlers/__init__.py
+++ src/payload_handlers/__init__.py
@@ -1,2 +1,14 @@
 from .factory import PayloadHandlerFactory
 
+from .handlers import \
+        BasePayloadHandler, \
+        DumpPayloadHandler, \
+        DCPPCPayloadHandler
+
+__all__ = [
+        'PayloadHandlerFactory',
+        'BasePayloadHandler',
+        'DumpPayloadHandler',
+        'DCPPCPayloadHandler'
+]
+
