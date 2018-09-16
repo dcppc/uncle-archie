e7e350a43c83a1930eb9b6f39aafcbd92ad31bc8
Charles Reid <charlesreid1@gmail.com>
2018-09-15 18:40:24 -0700

    fix up tasks init

--- src/tasks/__init__.py
+++ src/tasks/__init__.py
@@ -1,3 +1,22 @@
-from .base import UncleArchieTask, GithubTask, PyGithubTask
+# base Task types
+from .base import UncleArchieTask
+from .github_base import GithubTask, PyGithubTask
 
+# dcppc Task types
+from .private_www_PR_builder import private_www_PR_builder
+from .private_www_deployer import private_www_deployer 
+from .private_www_submodule_integration_PR_builder import private_www_submodule_integration_PR_builder 
+from .private_www_submodule_update_PR_opener import private_www_submodule_update_PR_opener 
+from .use_case_library_PR_builder import use_case_library_PR_builder 
+from .use_case_library_deployer import use_case_library_deployer 
 
+__all__ = [
+        'base',
+        'github_base',
+        'private_www_PR_builder',
+        'private_www_deployer',
+        'private_www_submodule_integration_PR_builder',
+        'private_www_submodule_update_PR_opener',
+        'use_case_library_PR_builder',
+        'use_case_library_deployer',
+]
