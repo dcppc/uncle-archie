285305fd6d5a1be385bdc9d05cd666071b6b0527
Charles Reid <charlesreid1@gmail.com>
2018-09-16 12:36:48 -0700

    add case of no handler type specified (use default)

--- src/payload_handlers/factory.py
+++ src/payload_handlers/factory.py
@@ -11,7 +11,11 @@ class PayloadHandlerFactory(object):
     }
     def factory(self,handler_type,config,**kwargs):
         if handler_type in self.payload_handlers.keys():
+            # return user-specified payload handler type
             return self.payload_handlers[handler_type](config,**kwargs)
+        elif handler_type=='':
+            # empty payload handler type => default
+            return self.payload_handlers['default'](config,**kwargs)
         else:
             err = "ERROR: PayloadHandlerFactory: factory(): "
             err += "Received an invalid handler type \"%s\"\n"%handler_type
