df6d1e7e425a53d81a6d6e0f8c40ddbfed370a61
Charles Reid <charlesreid1@gmail.com>
2018-09-17 13:47:58 -0700

    fix naming issue with github test task

--- src/tasks/__init__.py
+++ src/tasks/__init__.py
@@ -3,7 +3,7 @@ from .base import UncleArchieTask, LoggingTask
 from .github_base import GithubTask, PyGithubTask
 
 # test Task types
-from .gitub_test import TestPRTask
+from .ghtest import TestPRTask
 
 # dcppc Task types
 from .private_www_PR_builder import private_www_PR_builder
