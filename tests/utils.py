a72a216b2cd91c1cf41c61c1dbea4b4def0d7a62
Charles Reid <charlesreid1@gmail.com>
2018-09-17 15:40:16 -0700

    expand the types of pull requests we are sending

--- tests/utils.py
+++ tests/utils.py
@@ -10,6 +10,22 @@ HEADERS = {
 base = os.path.split(os.path.abspath(__file__))[0]
 
 
+######################################
+# Load and extract payloads
+
+
+def load_from_museum(fname):
+    """
+    Load a payload named fname from ../museum
+    """
+    fname = os.path.join(base,'..','museum',fname)
+    print("Opening file %s"%(fname))
+    d = {}
+    with open(fname,'r') as f:
+        d = json.load(f)
+    return d
+
+
 def extract_payload(request):
     """
     Given the reult of a flask client request,
@@ -18,6 +34,10 @@ def extract_payload(request):
     result = request.data.decode('utf-8')
     d = json.loads(result)
     return d
+
+
+######################################
+# Functions to post webhooks
 
 
 def post_pingpong_webhook(client):
@@ -40,14 +60,10 @@ def post_pingpong_webhook(client):
 
 def post_pr_webhook(client):
     """
-    Post a fake pull request webhook
+    Fake pull request webhook
     """
     # Get webhook from file
-    fname = os.path.join(base,'..','museum','pr_sync.json')
-    print("Opening file %s"%(fname))
-    d = {}
-    with open(fname,'r') as f:
-        d = json.load(f)
+    d = load_from_museum('pr_sync.json')
 
     # Post the webhook
     return client.post(
@@ -55,4 +71,64 @@ def post_pr_webhook(client):
             json=d,
             headers=HEADERS,
     )
+
+
+def post_new_branch_webhook(client):
+    """
+    Commit that creates a new branch
+    """
+    # Get webhook from file
+    d = load_from_museum('new_branch_event.json')
+
+    # Post the webhook
+    return client.post(
+            '/',
+            json=d,
+            headers=HEADERS,
+    )
+
+
+def post_close_pr_webhook(client):
+    """
+    Commit that closes a pull request
+    """
+    # Get webhook from file
+    d = load_from_museum('push_pr_to_master_1.json')
+
+    # Post the webhook
+    return client.post(
+            '/',
+            json=d,
+            headers=HEADERS,
+    )
+
+
+def post_pr_commit_to_master_webhook(client):
+    """
+    Commit that merges a pull request onto master
+    """
+    # Get webhook from file
+    d = load_from_museum('push_pr_to_master_2.json')
+
+    # Post the webhook
+    return client.post(
+            '/',
+            json=d,
+            headers=HEADERS,
+    )
+
+def post_pr_sync_webhook(client):
+    """
+    Commit that syncs an existing PR
+    """
+    # Get webhook from file
+    d = load_from_museum('sync_pr_event.json')
+
+    # Post the webhook
+    return client.post(
+            '/',
+            json=d,
+            headers=HEADERS
+    )
+
 
