74f93ce0e4c2a56de741ef0ba9af67fad80ea309
Charles Reid <charlesreid1@gmail.com>
2018-09-17 14:17:20 -0700

    move examples to example dir

--- tests/test_payload.py
+++ tests/test_payload.py
@@ -3,38 +3,56 @@ import archie
 import logging
 import os, sys
 import json
+import unittest
 
-def test_webhooks_ok():
-    archie.webapp.app.config['debug'] = True
-    archie.webapp.app.config['DEBUG'] = True
-    archie.webapp.app.config['TESTING'] = True
-    client = archie.webapp.app.test_client()
-    archie.webapp.app.set_payload_handler('')
+class TestPayload(unittest.TestCase):
+    def test_webhooks_ok(self):
+        """
+        Test that the Uncle Archie webhook server returns status 200
+        """
+        archie.webapp.app.config['debug'] = True
+        archie.webapp.app.config['DEBUG'] = True
+        archie.webapp.app.config['TESTING'] = True
+        client = archie.webapp.app.test_client()
+        archie.webapp.app.set_payload_handler('')
+    
+        r = post_pr_webhook(client)
+        self.assertEqual(r.status_code,200)
+    
+    def test_ping_webhook(self):
+        """
+        Test that the Uncle Archie webhook server plays ping pong
+        """
+        archie.webapp.app.config['DEBUG'] = True
+        archie.webapp.app.config['TESTING'] = True
+        client = archie.webapp.app.test_client()
+        archie.webapp.app.set_payload_handler('')
+    
+        r = post_pingpong_webhook(client)
+        self.assertEqual(r.status_code,200)
+    
+        d = extract_payload(r)
+        self.assertIn('msg',d.keys())
+        self.assertEqual(d['msg'],'pong')
+    
+    def test_pr_webhooks_ok(self):
+        """
+        Test that the Uncle Archie webhook server correctly processes PR webhooks
+        """
+        # assertLogs
+        # https://docs.python.org/3/library/unittest.html#unittest.TestCase.assertLogs
+        archie.webapp.app.config['DEBUG'] = True
+        archie.webapp.app.config['TESTING'] = True
+        client = archie.webapp.app.test_client()
+        archie.webapp.app.set_payload_handler('pr_test')
+    
+        with self.assertLogs() as cm:
+            r = post_pr_webhook(client)
+            self.assertEqual(r.status_code,200)
+    
+        self.assertIn(cm.output,'Pull Request')
+        
 
-    r = post_pr_webhook(client)
-    assert r.status_code==200
 
-def test_ping_webhook():
-    archie.webapp.app.config['debug'] = True
-    archie.webapp.app.config['DEBUG'] = True
-    archie.webapp.app.config['TESTING'] = True
-    client = archie.webapp.app.test_client()
-    archie.webapp.app.set_payload_handler('')
 
-    r = post_pingpong_webhook(client)
-    assert r.status_code==200
-
-    d = extract_payload(r)
-    assert 'msg' in d.keys()
-    assert d['msg']=='pong'
-
-def test_pr_webhooks_ok():
-    archie.webapp.app.config['debug'] = True
-    archie.webapp.app.config['DEBUG'] = True
-    archie.webapp.app.config['TESTING'] = True
-    client = archie.webapp.app.test_client()
-    archie.webapp.app.set_payload_handler('pr_test')
-
-    r = post_pr_webhook(client)
-    assert r.status_code==200
 
