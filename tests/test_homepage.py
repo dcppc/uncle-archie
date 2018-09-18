f1619cc43d7106fae94d74251ba1e0a0d2a0b814
Charles Reid <charlesreid1@gmail.com>
2018-09-17 20:05:18 -0700

    update how uncle archie webapp instantiated

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -7,10 +7,11 @@ def visit_homepage(client):
     return client.get('/')
 
 def test_visit_homepage():
-    archie.webapp.app.config['DEBUG'] = True
-    archie.webapp.app.config['TESTING'] = True
-    client = archie.webapp.app.test_client()
-    archie.webapp.app.set_payload_handler('')
+    app = archie.webapp.get_flask_app()
+    app.config['DEBUG'] = True
+    app.config['TESTING'] = True
+    client = app.test_client()
+    app.set_payload_handler('')
     r = visit_homepage(client)
-    print(r.data)
     assert b'Uncle Archie' in r.data
+
