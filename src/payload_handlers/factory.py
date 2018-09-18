ed4516545a25c5d073387699f8d1306e6e87518c
Charles Reid <charlesreid1@gmail.com>
2018-09-17 17:13:43 -0700

    add docstring to payload handler factory

--- src/payload_handlers/factory.py
+++ src/payload_handlers/factory.py
@@ -12,12 +12,19 @@ class PayloadHandlerFactory(object):
             'dcppc':   DCPPCPayloadHandler
     }
     def factory(self,handler_type,config,**kwargs):
+        """
+        Given a payload handler type 
+        (must be key in dictionary above),
+        create and return an instance of it.
+        """
         if handler_type in self.payload_handlers.keys():
             # return user-specified payload handler type
             return self.payload_handlers[handler_type](config,**kwargs)
+
         elif handler_type=='':
             # empty payload handler type => default
             return self.payload_handlers['default'](config,**kwargs)
+
         else:
             err = "ERROR: PayloadHandlerFactory: factory(): "
             err += "Received an invalid handler type \"%s\"\n"%handler_type
