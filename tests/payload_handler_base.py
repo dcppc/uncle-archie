021e598c7d60053b8bb2aa53612bf8d9915219f0
Charles Reid <charlesreid1@gmail.com>
2018-09-24 00:25:00 -0400

    we should be checking for 1+ log statements

--- tests/payload_handler_base.py
+++ tests/payload_handler_base.py
@@ -16,5 +16,6 @@ class payload_handler_base(unittest.TestCase):
         
     def verify_in_logs(self,client,func,log_statements):
         r = func(client)
+        self.assertTrue(len(log_statements)>0)
         for log_statement in log_statements:
             self.assertLogs(log_statement)
