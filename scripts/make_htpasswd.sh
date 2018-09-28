2d2f2e025e494d7d1917192d00647caae04a9f82
Florence Python <fp9695253@gmail.com>
2018-09-28 10:46:12 -0700

    clean up htpasswd script

--- scripts/make_htpasswd.sh
+++ scripts/make_htpasswd.sh
@@ -22,30 +22,70 @@ function usage() {
 }
 
 
-function copy() {
+function final() {
     echo ""
-    echo "Successfully output .htpasswd file in current directory."
-    echo "To use this file with nginx, place file at /etc/nginx/.htpasswd"
-    echo "and add the following lines to the appropriate nginx conf block:"
-    echo "    location / { "
-    echo "        ..."
-    echo "        auth_basic \"Restricted Content\";"
-    echo "        auth_basic_user_file /etc/nginx/.htpasswd;"
-    echo "        ..."
-    echo "    }"
+    echo "make_htpasswd.sh:"
+    echo ""
+    echo "    Successfully output .htpasswd file in current directory."
+    echo ""
+    echo "    To use this file with nginx, place file at /etc/nginx/.htpasswd"
+    echo "    and add the following lines to the appropriate nginx conf block:"
+    echo ""
+    echo "        location / { "
+    echo "            ..."
+    echo "            auth_basic \"Restricted Content\";"
+    echo "            auth_basic_user_file /etc/nginx/.htpasswd;"
+    echo "            ..."
+    echo "        }"
     echo ""
     exit 1;
 }
 
 
+## Only run this script if you are root!
+#if [ "$(id -u)" != "0" ]; then
+#    echo ""
+#    echo ""
+#    echo "This script should be run as root."
+#    echo ""
+#    echo ""
+#    usage;
+#    exit 1;
+#fi
+
+
+# User must set NGINX_PASSWORD using environment variable
 if [[ "${NGINX_PASSWORD}" == "" ]]; then
+
+    # User is confused
     usage
+
 else
+
+    echo "+---------------------------+"
+    echo "Creating .htpasswd file..."
+    echo "STEP 1: Preparing to set username"
+
+    # Username is always "archie"
     echo -n 'archie:' >> .htpasswd
-    openssl passwd -apr1 >> .htpasswd
+
+    echo "STEP 1: Username has been set."
+    echo "STEP 2: Preparing to set password for user 'archie'."
+
+    # Password comes from env var
+    openssl passwd -apr1 ${NGINX_PASSWORD} >> .htpasswd
+
+    echo "STEP 2: Password has been set."
+    echo "STEP 3: Preparing to copy .htpasswd file into place."
+
+    # Move the .htpasswd file into place
     if [ -f /etc/nginx/ ]; then
-        mv .htpasswd /etc/nginx/.htpasswd
+        sudo mv .htpasswd /etc/nginx/.htpasswd
     fi
+
+    echo "STEP 3: .htpasswd file has been moved into place."
+
+    final
 fi
 
 
