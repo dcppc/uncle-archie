8f723c0271b4e5cce6d4a20b359935768a0018d3
C Reid <charlesreid1@Gmail.com>
2018-11-20 17:12:33 -0800

    add the simplest test: endpoint /

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -3,15 +3,12 @@ import os
 import tempfile
 import logging
 
-def visit_homepage(client):
-    return client.get('/')
-
 def test_visit_homepage():
     app = archie.webapp.get_flask_app()
     app.config['DEBUG'] = True
     app.config['TESTING'] = True
     client = app.test_client()
     app.set_payload_handler('')
-    r = visit_homepage(client)
+    r = client.get('/')
     assert b'Uncle Archie' in r.data
 
